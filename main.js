9607e25c5659dc4749b1f4ff6435e0306ad6deca
Samuelodan <samodan195@gmail.com>
2022-02-22 01:23:00 +0100

    Write function to accept player move

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const computerPlay = () => {
     let random = Math.floor(Math.random()*3) + 1;
-    let botMove = 0;
+    let botMove;
     if (random === 1){
         botMove = "rock";
     } else if (random === 2){
@@ -8,8 +8,26 @@ const computerPlay = () => {
     } else if (random === 3){
         botMove = "scissors";
     }
-
     return botMove;
 };
-
 computerPlay();
+
+const player = () => {
+    let choice = prompt("Make your move. 'rock', 'paper' or 'scissors'").toLowerCase();
+    let move;
+    if (choice === "rock"){
+        move = "rock";
+    } else if (choice === "paper"){
+        move = "paper";
+    } else if (choice === "scissors"){
+        move = "scissors";
+    } else {
+        alert("enter a valid choice!");
+        return player();
+    }
+
+    return move;
+}
+player();
+
+
