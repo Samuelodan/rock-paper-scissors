d98b598a3cc7f4a608014548b88225903b4f3b2e
Samuelodan <samodan195@gmail.com>
2022-02-22 17:13:23 +0100

    Write match function to compare player moves.
    this function will compare moves and return the outcome as a popup.
    it will also restart the round each time

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const computerPlay = () => {
+
+
+let botMove = "";
+function computerPlay (){
     let random = Math.floor(Math.random()*3) + 1;
-    let botMove;
+    // let botMove;
     if (random === 1){
         botMove = "rock";
     } else if (random === 2){
@@ -10,11 +13,12 @@ const computerPlay = () => {
     }
     return botMove;
 };
-computerPlay();
+// computerPlay();
 
-const player = () => {
+let move = "";
+function player (){
     let choice = prompt("Make your move. 'rock', 'paper' or 'scissors'").toLowerCase();
-    let move;
+    // let move;
     if (choice === "rock"){
         move = "rock";
     } else if (choice === "paper"){
@@ -28,6 +32,50 @@ const player = () => {
 
     return move;
 }
-player();
 
 
+function match (botMove, move){
+    alert(`Computer: ${botMove}, You: ${move}`);
+
+    if (botMove === move){
+        alert("Tie!! Make another Move.");
+        startMatch();
+        
+        //win cases
+    } else if (botMove === "rock" && move === "paper"){
+        alert("You win! Play again.")
+        startMatch();
+
+    } else if (botMove === "scissors" && move === "rock"){
+        alert("You win! Play again.")
+        startMatch();
+
+    } else if (botMove === "paper" && move === "scissors"){
+        alert("You win! Play again")
+        startMatch();
+        
+        // lose cases
+    } else if (botMove === "paper" && move === "rock"){
+        alert("You lost this round! Make another move")
+        startMatch();
+
+    } else if (botMove === "rock" && move === "scissors"){
+        alert("You lost this round! Make another move")
+        startMatch();
+
+    } else if (botMove === "scissors" && move === "paper"){
+        alert("You lost this round! Make another move")
+        startMatch();
+    }
+}
+
+// match(botMove, move);
+
+
+// function startMatch  (){
+//     computerPlay();
+//     player();
+//     match(botMove, move);
+// }
+
+// startMatch();
