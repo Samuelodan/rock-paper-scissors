791ff960795c639dc810ca45fbf45347893e7429
Samuelodan <samodan195@gmail.com>
2022-03-03 19:23:39 +0100

    Add move button function and score-count updater
    The buttons for making moves now work, score count is now displayed after every round, and some html elements get new ids.
    
    startMatch function no longer plays five rounds in a row.

--- main.js
+++ main.js
@@ -12,65 +12,71 @@ function computerPlay (){
 };
 
 let move = "";
-function player (){
-    let choice = prompt("Make your move. 'rock', 'paper' or 'scissors'").toLowerCase();
+function player (choice){
+    // let choice = prompt("Make your move. 'rock', 'paper' or 'scissors'").toLowerCase();
     if (choice === "rock"){
         move = "rock";
     } else if (choice === "paper"){
         move = "paper";
     } else if (choice === "scissors"){
         move = "scissors";
-    } else {
+    }; /*else {
         alert("enter a valid choice!");
         return player();
-    }
+    }*/
 
     return move;
 }
 
 function startRound(){
     computerPlay();
-    player();
     alert(`Computer: ${botMove}, You: ${move}`);
-
     if (botMove === move){
+        // roundHeaderElement.textContent = "It's a tie";
+        // roundWinnerElement.textContent = "Make Another Move"
         alert("Tie!! Make another Move.");
         
         //win cases
     } else if (botMove === "rock" && move === "paper"){
+
         alert("You win! Paper beats Rock! Play again.")
         userScore++;
+        userScoreElement.textContent = `${userScore}`
 
     } else if (botMove === "scissors" && move === "rock"){
         alert("You win! Rock beats Scissors! Play again.")
         userScore++;
+        userScoreElement.textContent = `${userScore}`
 
     } else if (botMove === "paper" && move === "scissors"){
         alert("You win! Scissors beat Paper! Play again")
         userScore++;
+        userScoreElement.textContent = `${userScore}`
         
         // lose cases
     } else if (botMove === "paper" && move === "rock"){
         alert("You lost this round! Paper beats Rock! Try again")
         botScore++;
+        botScoreElement.textContent = `${botScore}`
 
     } else if (botMove === "rock" && move === "scissors"){
         alert("You lost this round! Rock beats Scissors! Try again")
         botScore++;
+        botScoreElement.textContent = `${botScore}`
 
     } else if (botMove === "scissors" && move === "paper"){
         alert("You lost this round! Scissors beat Paper! Try again")
         botScore++;
+        botScoreElement.textContent = `${botScore}`
         
     }
 }
 
 let botScore = 0;
 let userScore = 0;
+
 function startMatch(){
-    for (let i = 1; i <= 5; i++){
         startRound();
-    };
     let result = "";
     if (botScore < userScore){
         result = `Final Results \n\nYou win! You had: ${userScore} wins, Computer had: ${botScore} wins`;
@@ -79,9 +85,43 @@ function startMatch(){
     } else {
         result = `Final Results \n\nIt's a tie! You had: ${userScore} wins, Computer had: ${botScore} wins`;
     }
-    alert(result);
-    botScore = 0;
-    userScore = 0;
+    // alert(result);
+    // botScore = 0;
+    // userScore = 0;
 }
 
-startMatch();
+//startMatch();
+
+//botMoveElements
+const botRockElement = document.getElementById("bot-rock");
+const botPaperElement = document.getElementById("bot-paper");
+
+
+const roundHeaderElement = document.querySelector("#round-winner h3");
+const roundWinnerElement = document.querySelector("#round-winner h2");
+
+const botScoreElement = document.getElementById("bot-score");
+const userScoreElement = document.getElementById("user-score");
+
+//chooseMoves
+const rockButton = document.getElementById("rock-button");
+const paperButton = document.getElementById("paper-button");
+const scissorsButton = document.getElementById("scissors-button");
+
+rockButton.addEventListener("click", () => {
+    player("rock");
+    startMatch()
+    
+});
+paperButton.addEventListener("click", () => {
+    player("paper");
+    startMatch()
+    
+});
+scissorsButton.addEventListener("click", () => {
+    player("scissors");
+    startMatch()
+    
+});
+
+
