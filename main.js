f9d6059ddfdf8296cbf586d3d66ea9c1d47783c6
Samuelodan <samodan195@gmail.com>
2022-03-04 18:14:48 +0100

    Hide and show move-icons after each move

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 let botMove = "";
 function computerPlay (){
     let random = Math.floor(Math.random()*3) + 1;
+    let botId = ""
     if (random === 1){
         botMove = "rock";
+        botId = "bot-rock";
     } else if (random === 2){
         botMove = "paper";
+        botId = "bot-paper";
     } else if (random === 3){
         botMove = "scissors";
+        botId = "bot-scissors";
     }
-    return botMove;
+
+    //showBotIcons(botId)
+    showIcons(botId);
+    // return botMove;
 };
 
 let move = "";
 function player(choice){
-    // let choice = prompt("Make your move. 'rock', 'paper' or 'scissors'").toLowerCase();
+    let id = "";
+
     if (choice === "rock"){
         move = "rock";
+        id = "user-rock"
+
     } else if (choice === "paper"){
         move = "paper";
+        id = "user-paper"
     } else if (choice === "scissors"){
         move = "scissors";
+        id = "user-scissors"
     }; /*else {
         alert("enter a valid choice!");
         return player();
     }*/
-    return move;
+    // return move;
+
+    showIcons(id);
 }
 
 function startRound(){
     computerPlay();
-    alert(`Computer: ${botMove}, You: ${move}`);
+    // alert(`Computer: ${botMove}, You: ${move}`);
     if (botMove === move){
         roundHeaderElement.textContent = "";
         roundWinnerElement.textContent = "It's a tie for this round!"
@@ -101,6 +115,26 @@ function startRound(){
 let botScore = 0;
 let userScore = 0;
 
+// function waitForStart(){
+//     document.querySelector(".move-button-div").classList.toggle("unsee");
+//     newGameButton.classList.toggle("unsee")
+// }
+//waitForStart();
+
+// function startNewGame(){
+//     resultElement.textContent = "";
+//     roundHeaderElement.textContent = "";
+//     roundWinnerElement.textContent = ""
+//     roundFooterElement.textContent = ""
+//     botScore = 0;
+//     userScore = 0;
+//     botScoreElement.textContent = `${botScore}`
+//     userScoreElement.textContent = `${botScore}`
+//     document.querySelector(".move-button-div").classList.toggle("unsee");
+//     newGameButton.classList.toggle("unsee")
+//     hideIcons();
+// }
+
 function getWinner(){
         // startRound();
     let result = "";
@@ -115,11 +149,31 @@ function getWinner(){
     resultElement.textContent = result;
     const brElement = document.createElement("br");
     resultElement.appendChild(brElement);
-    resultElement.appendChild(document.createTextNode("start a new game")); 
-    // alert(result);
-    // botScore = 0;
-    // userScore = 0;
+    resultElement.appendChild(document.createTextNode("start a new game"));
+
+    // waitForStart()
+
+    setTimeout(() => {
+
+    }, 5000)
 }
+
+function hideIcons() {
+    const moveIcons = document.querySelectorAll(".show-move img");
+    for (icon of moveIcons){
+        if (!icon.classList.contains("hidden")){
+            icon.classList.add("hidden")
+        }
+    }
+};
+
+//hideIcons();
+
+function showIcons(id) {
+    document.getElementById(`${id}`).classList.remove("hidden");
+}
+
+
 
 //startMatch();
 
@@ -137,24 +191,37 @@ const userScoreElement = document.getElementById("user-score");
 
 const resultElement = document.getElementById("result");
 
+const newGameButton = document.querySelector(".fourth button");
+
 //chooseMoves
 const rockButton = document.getElementById("rock-button");
 const paperButton = document.getElementById("paper-button");
 const scissorsButton = document.getElementById("scissors-button");
 
+
+
+
+
+
+
+
 rockButton.addEventListener("click", () => {
+    hideIcons()
+
     player("rock");
     resultElement.textContent = "";
     setTimeout(startRound, );
     
 });
 paperButton.addEventListener("click", () => {
+    hideIcons()
     player("paper");
     resultElement.textContent = "";
     setTimeout(startRound, );
     
 });
 scissorsButton.addEventListener("click", () => {
+    hideIcons()
     player("scissors");
     resultElement.textContent = "";
     setTimeout(startRound, );
@@ -162,3 +229,4 @@ scissorsButton.addEventListener("click", () => {
 });
 
 
+newGameButton.addEventListener("click", startNewGame)
